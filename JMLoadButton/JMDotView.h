e7b9d292285fbce559c8f216627746985e5dedac
Jeremy <594702537@qq.com>
2016-08-26 21:36:37 +0800

    1. 卡券添加

---
+++ JMLoadButton/JMDotView.h
@@ -0,0 +1,13 @@
+//
+//  JMDotView.h
+//  JMLoadButton
+//
+//  Created by 萧锐杰 on 16/8/26.
+//  Copyright © 2016年 萧锐杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMDotView : UIView
+
+@end
---
+++ JMLoadButton/JMDotView.m
@@ -0,0 +1,14 @@
+//
+//  JMDotView.m
+//  JMLoadButton
+//
+//  Created by 萧锐杰 on 16/8/26.
+//  Copyright © 2016年 萧锐杰. All rights reserved.
+//
+
+#import "JMDotView.h"
+
+@implementation JMDotView
+
+
+@end
